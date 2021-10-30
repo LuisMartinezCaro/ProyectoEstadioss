bb7f2ecc8b2636cb73faa8f894a233b432861e71
LuisMartinezCaro <martinesluis1916@gmail.com>
2021-10-30 06:40:52 -0300

    Initial commit

---
+++ app/src/main/java/cl/luis/proyectoestadios/arica2.java
@@ -0,0 +1,30 @@
+package cl.luis.proyectoestadios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class arica2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arica2);
+
+    }
+
+    public void ButtonComoLlegar (View view){
+        Intent buttonComoLlegar = new Intent(this, gps.class);
+        startActivity(buttonComoLlegar);
+
+
+    }
+    public void MiUbicaion (View view){
+        Intent miUbicaion = new Intent(this, Localizacion.class);
+        startActivity(miUbicaion);
+
+
+    }
+}//MiUbicaion
